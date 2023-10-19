1966c093b97381d9cd50a286a50402596ac91db7
Julia Glinka <julia.w.glinka@gmail.com>
2023-10-19 17:02:03 +0200

    Add template of booking widget to BOOKING subpage

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,10 +1,31 @@
+import { templates } from "../settings.js";
+import utils from "../utils.js";
 
 
 class Booking {
-    constructor(){
-        //const thisBooking =  this;
+    constructor(container){
+        const thisBooking =  this;
+        
+        thisBooking.element = container;
 
+        thisBooking.render(container);
+        //thisBooking.initWidgets();
+    }
 
+    render(container){
+        const thisBooking = this;
+        console.log(this);
+
+        const generatedHTML = templates.bookingWidget();                     //nie wpisujemy danych w nawiasie bo nie potrzebujemy nic dodawac do tego szablonu-brak placeholderow
+        console.log('generatedHTML',generatedHTML);
+
+        thisBooking.dom = {};
+
+        thisBooking.dom.wrapper = container;
+
+        thisBooking.dom.wrapper.innerHTML = generatedHTML;      
+
+        
     }
 
 }
