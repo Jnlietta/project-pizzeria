af10f6f55d7b69b12e4aed60f897d8709c5cde39
Julia Glinka <julia.w.glinka@gmail.com>
2023-10-19 20:54:18 +0200

    Add to code getter and setter

--- dist/js/components/BaseWidget.js
+++ dist/js/components/BaseWidget.js
@@ -7,10 +7,16 @@ class BaseWidget {
         thisWidget.dom = {};
         thisWidget.dom.wrapper = wrapperElement;
 
-        thisWidget.value = initialValue;
+        thisWidget.correctValue = initialValue;
     }
 
-    setValue(value){
+    get value(){ //getter - metoda wykonywana przy kazdej próbie odczytywania wartości właściwości 'value'
+        const thisWidget = this;
+
+        return thisWidget.correctValue;
+    }
+
+    set value(value){ //setter - metoda która jest wykonywana przy kazdej próbie ustawienia nowej wartosci właściwości 'value'
         const thisWidget = this;
   
         const newValue = thisWidget.parseValue(value);
@@ -18,13 +24,19 @@ class BaseWidget {
         //console.log('value',value);
         
         /* TODO: Add validation */
-        if(thisWidget.value!== newValue && thisWidget.isValid(newValue)){ //dlaczego jest taki operator  a nie != ??     /* dlaczego nie? :newValue!=null    / dlaczego nie Number.isNaN()?
-        thisWidget.value = newValue; // czy cos sie stanie jak zamienimy miejscami te rzeczy wzgledem znaku rowna sie?
+        if(thisWidget.correctValue!== newValue && thisWidget.isValid(newValue)){ //dlaczego jest taki operator  a nie != ??     /* dlaczego nie? :newValue!=null    / dlaczego nie Number.isNaN()?
+        thisWidget.correctValue = newValue; // czy cos sie stanie jak zamienimy miejscami te rzeczy wzgledem znaku rowna sie?
         
         thisWidget.announce();
         }
   
         thisWidget.renderValue();
+    }
+
+    setValue(value){
+        const thisWidget = this;
+
+        thisWidget.value = value;
     }
 
     parseValue(value){
